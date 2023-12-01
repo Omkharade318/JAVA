3d0c173faad7dd2f76c62441e5f4cb8a4b0cbd47
Om kharade <146190558+Omkharade318@users.noreply.github.com>
2023-12-01 20:06:44 +0530

    Add files via upload
    sum of two numbers without using arithmetic operators

---
+++ Sum_without_using_Arithmatic_Opaerators.java
@@ -0,0 +1,8 @@
+public class Sum_without_using_Arithmatic_Opaerators {
+    public static void main(String[] args) {
+        int a=5;
+        int b=3;
+        for(int i= 1; i<=b; i++ ){a++;}
+        System.out.println(a);
+    }
+}
